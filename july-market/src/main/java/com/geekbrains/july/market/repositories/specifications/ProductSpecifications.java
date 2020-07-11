be7c839e96f23ca14d8ce0c4fd569eade271d8ca
dagov42 <dmitriy@govorukhin.xyz>
2020-07-11 16:08:27 +0300

    Добавил строку поиска, но сам поиск не смог реализовать

---
+++ july-market/src/main/java/com/geekbrains/july/market/repositories/specifications/ProductSpecifications.java
@@ -0,0 +1,18 @@
+package com.geekbrains.july.market.repositories.specifications;
+
+import com.geekbrains.july.market.entities.Product;
+import org.springframework.data.jpa.domain.Specification;
+
+public class ProductSpecifications {
+    public static Specification<Product> priceGreaterOrEqualsThan(int minPrice) {
+        return (Specification<Product>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.greaterThanOrEqualTo(root.get("price"), minPrice);
+    }
+
+    public static Specification<Product> priceLesserOrEqualsThan(int maxPrice) {
+        return (Specification<Product>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(root.get("price"), maxPrice);
+    }
+
+    public static Specification<Product> findByTitle(String title) {
+        return (Specification<Product>) (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(criteriaBuilder.lower(root.get("title")), "%" + title.toLowerCase() + "%");
+    }
+}
